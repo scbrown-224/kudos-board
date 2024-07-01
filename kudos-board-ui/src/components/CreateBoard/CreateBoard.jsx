3a33fdd9f219cba2578194c98bfa759c18fd56f9
Jazlyn <jonesjazlyn12@gmail.com>
2024-07-01 12:03:29 -0700

    Updated the FrontEnd
    - deleted the extra folder
    - created a template

---
+++ kudos-board-ui/src/components/CreateBoard/CreateBoard.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import React from "react";
+import "./CreateBoard.css";
+const CreateBoard = () => {
+  return (
+      <div className="CreateBoard">
+
+      </div>
+  );
+};
+export default CreateBoard;
